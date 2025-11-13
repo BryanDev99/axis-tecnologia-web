bff9310365833a427e8fc88c71b59e64b4e2dd1d
bragancaDev9 <bryanstos99@gmail.com>
2025-11-13 16:26:06 -0300

    Mini alteração para apagar os campos após o envio da mensagem.

--- js/home-scripts.js
+++ js/home-scripts.js
@@ -1,5 +1,12 @@
 const btnSubmit = document.querySelector("#btn-submit");
 
 btnSubmit.addEventListener("mousedown", (e) => {
+  e.preventDefault();
+
+  document.querySelector("#name").value = "";
+  document.querySelector("#email").value = "";
+  document.querySelector("#message").value = "";
+  
   alert("Mensagem enviada! Aguarde a resposta em até 4 horas!");
 });
+
